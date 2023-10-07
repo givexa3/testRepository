ff9ab28b4d2e321572e80482247367e43fa17525
givexa3 <gugakhutsishvili@yahoo.com>
2023-10-07 21:18:50 +0400

    main commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,35 @@
 package org.example;
 
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+
+        User user1 = new User("USER1", 23, null, new UserDetails("Anabana"));
+        User user2 = new User("USER2", 25, null, new UserDetails("GIVEXA"));
+        User user3 = new User("USER3", 27, null, new UserDetails("Zoro"));
+
+        List<User> userList = new ArrayList<>();
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+
+        List<User> result = userList
+                .stream()
+                .sorted(Comparator.comparing(u -> u.getUserDetails().getFullName()))
+                .collect(Collectors.toList());
+
+        List<User> result2 = userList
+                .stream()
+                .sorted(Comparator.comparing(u -> u.getUserDetails().getFullName(), Comparator.reverseOrder()))
+                .collect(Collectors.toList());
+
+        result.forEach(d-> System.out.println(d.getName()));
+
+        System.out.println("Result 2: ");
+
+        result2.forEach(d-> System.out.println(d.getName()));
     }
 }
